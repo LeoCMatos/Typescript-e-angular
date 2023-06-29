c7a046784858afa38b5faf9957ea312c0346d4ad
Leonardo Matos <leocamatos@outlook.com>
2023-06-29 19:02:03 -0300

    update

--- angular/projetos/app2/src/app/app.module.ts
+++ angular/projetos/app2/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TesteComponent } from './components/teste/teste.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TesteComponent,
-    ParentDataComponent
+    ParentDataComponent,
+    IfRenderComponent
   ],
   imports: [
     BrowserModule,
